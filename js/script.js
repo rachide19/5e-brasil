d63fa16d629d8242d3246e68614dcd30626e2726
Lucas Rachide <rachide17@gmail.com>
2025-06-14 11:18:46 -0300

    Versão inicial do site

---
+++ js/script.js
@@ -0,0 +1,45 @@
+[
+  {
+    "name": "Adepto Marcial",
+    "prerequisite": "Nenhum",
+    "source": "PHB",
+    "entries": [
+      "Você treinou extensivamente com uma variedade de armas, ganhando os seguintes benefícios:",
+      {
+        "type": "list",
+        "items": [
+          "Você ganha proficiência com quatro armas de sua escolha. Cada uma deve ser uma arma simples ou marcial.",
+          "Você aprende duas manobras de sua escolha do arquétipo de Guerreiro, o Mestre de Batalha.",
+          "Se uma manobra que você usar exigir que seu alvo faça um teste de resistência para resistir aos efeitos da manobra, a CD do teste de resistência é igual a 8 + seu bônus de proficiência + seu modificador de Força ou Destreza (sua escolha)."
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Atirador Aguçado",
+    "prerequisite": "Nenhum",
+    "source": "PHB",
+    "entries": [
+      "Você dominou o uso de armas de longo alcance, ganhando os seguintes benefícios:",
+      {
+        "type": "list",
+        "items": [
+          "Atacar um alvo a longa distância não impõe desvantagem em suas jogadas de ataque com armas de longo alcance.",
+          "Suas armas de longo alcance ignoram meia cobertura e três-quartos de cobertura.",
+          "Antes de fazer um ataque com uma arma de longo alcance com a qual você é proficiente, você pode optar por sofrer uma penalidade de -5 na jogada de ataque. Se o ataque acertar, você adiciona +10 ao dano do ataque."
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sortudo",
+    "prerequisite": "Nenhum",
+    "source": "PHB",
+    "entries": [
+      "Você tem uma sorte inexplicável que parece surgir nos momentos certos.",
+      "Você tem 3 pontos de sorte. Sempre que fizer uma jogada de ataque, um teste de habilidade ou um teste de resistência, você pode gastar um ponto de sorte para rolar um d20 adicional. Você pode escolher usar seu ponto de sorte depois de rolar o dado, mas antes que o resultado seja determinado. Você escolhe qual dos d20s usar para a jogada de ataque, teste de habilidade ou teste de resistência.",
+      "Você também pode gastar um ponto de sorte quando uma jogada de ataque é feita contra você. Role um d20 e, em seguida, escolha se o ataque usa a jogada do atacante ou a sua. Se mais de uma criatura gastar um ponto de sorte para influenciar o resultado de uma rolagem, os pontos se cancelam mutuamente; nenhuma rolagem adicional é feita.",
+      "Você recupera seus pontos de sorte gastos quando termina um descanso longo."
+    ]
+  }
+]
